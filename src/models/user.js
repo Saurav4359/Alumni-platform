8c51d155a49baf8a5b4b054f05fc221b261f5d6a
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-12-02 06:21:48 +0530

    userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,27 @@
 import mongoose, { mongo } from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "alumni"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // it shows created and updated at....
+  }
+);
 const UserModel = mongoose.model("users", UserSchema);
 
 export { UserModel };
